c34e04bc4160813f10b4e5ea4c7cf5b669bb4acf
Sanskar Sakhareliya <Sansakhraliya@gmail.com>
2022-02-24 19:19:41 +0530

    Added active modal and created functions

--- shop/models.py
+++ shop/models.py
@@ -8,11 +8,32 @@ class Product(models.Model):
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
+    status = models.CharField(max_length=15, choices=STATUS, default='Active')
     pub_date = models.DateField()
     image = models.ImageField(upload_to="shop/images", default="")
 
+    def product_status(self):
+        # print(self.status)
+        if(self.status == "Active"):
+            return format_html(
+                '<span style="color: {};">{}</span>',
+                "green",
+                "Active",
+                )
+        else:
+            return format_html(
+                '<span style="color: {};">{}</span>',
+                "red",
+                "InActive",
+                )
+            
+    @property            
+    def product_image(self):
+        return mark_safe('<img src="{}" width="160" height="130" />'.format(self.image.url))
+
     def __str__(self):
         return self.product_name
+
 
 
 class Contact(models.Model):
@@ -29,6 +50,7 @@ class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
     items_json = models.CharField(max_length=5000)
     amount = models.IntegerField(default=0)
+    status = models.CharField(max_length=15, choices=STATUS, default='Active')
     name = models.CharField(max_length=90)
     email = models.CharField(max_length=111)
     address = models.CharField(max_length=111)
@@ -36,6 +58,21 @@ class Orders(models.Model):
     state = models.CharField(max_length=111)
     zip_code = models.CharField(max_length=111)
     phone = models.CharField(max_length=111, default="")
+
+    def Status(self):
+        # print(self.status)
+        if(self.status == "Active"):
+            return format_html(
+                '<span style="color: {};">{}</span>',
+                "green",
+                "Active",
+                )
+        else:
+            return format_html(
+                '<span style="color: {};">{}</span>',
+                "red",
+                "InActive",
+                )
 
 
 class OrderUpdate(models.Model):
