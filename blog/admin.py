399bb8408f486fc27e1fbdf6df8bce69023891e7
Sanskar Sakhareliya <Sansakhraliya@gmail.com>
2022-02-24 19:31:55 +0530

    Updated blog models and registered admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Blogpost
 
-admin.site.register(Blogpost)
+class BlogpostAdmin(admin.ModelAdmin):
+    list_display = ("post_id", "title", "blog_status", "pub_date", "blog_image")
+admin.site.register(Blogpost, BlogpostAdmin)\
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.utils.html import format_html
+from django.utils.html import mark_safe
+
+STATUS = (
+    ('Active', 'ACTIVE'),
+    ('InActive', 'INACTIVE'),
+)
 
 # Create your models here.
 class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
+    status = models.CharField(max_length=15, choices=STATUS, default='Active')
     head0 = models.CharField(max_length=500, default="")
     chead0 = models.CharField(max_length=5000, default="")
     head1 = models.CharField(max_length=500, default="")
@@ -12,6 +20,25 @@ class Blogpost(models.Model):
     chead2 = models.CharField(max_length=5000, default="")
     pub_date = models.DateField()
     thumbnail = models.ImageField(upload_to="blog/images", default="")
+
+    def blog_status(self):
+        # print(self.status)
+        if(self.status == "Active"):
+            return format_html(
+                '<span style="color: {};">{}</span>',
+                "green",
+                "Active",
+                )
+        else:
+            return format_html(
+                '<span style="color: {};">{}</span>',
+                "red",
+                "InActive",
+                )
+
+    @property            
+    def blog_image(self):
+        return mark_safe('<img src="{}" width="160" height="130" />'.format(self.thumbnail.url))
 
     def __str__(self):
         return self.title
